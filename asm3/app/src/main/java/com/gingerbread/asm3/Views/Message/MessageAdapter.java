985633c88f4a446599b587b50aa2fc6855cbd38f
Tkphuc <s3927187@rmit.edu.vn>
2025-01-03 07:40:15 +0700

    message items added

---
+++ asm3/app/src/main/java/com/gingerbread/asm3/Views/Message/MessageAdapter.java
@@ -0,0 +1,24 @@
+package com.gingerbread.asm3.Views.Message;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MessageAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder>{
+    @NonNull
+    @Override
+    public CustomAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
