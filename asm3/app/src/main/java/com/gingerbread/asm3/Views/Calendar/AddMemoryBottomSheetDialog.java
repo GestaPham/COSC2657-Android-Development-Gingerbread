e04c120abced3293a26f7a2dd9ab47df9140ed73
Tkphuc <s3927187@rmit.edu.vn>
2025-01-06 22:48:08 +0700

    image upload for memories

--- asm3/app/src/main/java/com/gingerbread/asm3/Views/Calendar/AddMemoryBottomSheetDialog.java
+++ asm3/app/src/main/java/com/gingerbread/asm3/Views/Calendar/AddMemoryBottomSheetDialog.java
@@ -1,6 +1,10 @@
 package com.gingerbread.asm3.Views.Calendar;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,9 +18,16 @@ import androidx.annotation.Nullable;
 
 import com.gingerbread.asm3.R;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 
 public class AddMemoryBottomSheetDialog extends BottomSheetDialogFragment {
-
+    private AddMemoryListener listener;
+    private Uri imageUri;
+    private static int PICK_IMAGE_REQ = 1;
+    private String uploadedImageUrl;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -34,7 +45,7 @@ public class AddMemoryBottomSheetDialog extends BottomSheetDialogFragment {
             String note = noteInput.getText().toString();
             String name = memoryNameInput.getText().toString();
 
-            listener.onMemoryAdded(name, note, date);
+            listener.onMemoryAdded(name, note, date,uploadedImageUrl);
             dismiss();
         });
         return v;
@@ -51,10 +62,40 @@ public class AddMemoryBottomSheetDialog extends BottomSheetDialogFragment {
     }
 
     public interface AddMemoryListener{
-        void onMemoryAdded(String name,String note,String date);
+        void onMemoryAdded(String name,String note,String date,String imageUrl);
 
     }
-    private AddMemoryListener listener;
+    private void uploadImage(){
 
+    }
+    private void openImagePicker(){
+        Intent imagePickerIntent = new Intent();
+        imagePickerIntent.setType("image/*");
+        imagePickerIntent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(imagePickerIntent,PICK_IMAGE_REQ);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQ && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imageUri = data.getData(); // This is the URI of the selected image
+        }
+    }
+    private void uploadImageToCloudStorage(){
+        if(imageUri != null){
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference();
 
+            String fileName = System.currentTimeMillis() + ".jpg";
+            StorageReference fileRef = storageRef.child("images/" + fileName);
+            fileRef.putFile(imageUri)
+                    .addOnSuccessListener(taskSnapshot->{
+                        fileRef.getDownloadUrl().addOnSuccessListener(uri->{
+                            uploadedImageUrl =uri.toString();
+                        });
+                    }).addOnFailureListener(e->{});
+        }else{
+
+        }
+    }
 }
--- asm3/app/src/main/java/com/gingerbread/asm3/Views/Calendar/CalendarActivity.java
+++ asm3/app/src/main/java/com/gingerbread/asm3/Views/Calendar/CalendarActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
@@ -124,7 +123,8 @@ public class CalendarActivity extends BaseActivity implements AddMemoryBottomShe
     }
 
     @Override
-    public void onMemoryAdded(String name, String note, String date) {
-        addNewMemory(name,note, String.valueOf(selectedDate),photoUrl, currentUser.getUid(),"");
+    public void onMemoryAdded(String name, String note, String date, String imageUrl) {
+        addNewMemory(name,note, date,imageUrl, currentUser.getUid(),"");
     }
+
 }
