e0d6a7fa47134f2c815c3f1ea71a07868f54bd44
Tkphuc <s3927187@rmit.edu.vn>
2025-01-09 14:20:03 +0700

    AddEventBottomSheetDialog added

---
+++ asm3/app/src/main/java/com/gingerbread/asm3/Views/Calendar/AddEventBottomSheetDialog.java
@@ -0,0 +1,33 @@
+package com.gingerbread.asm3.Views.Calendar;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class AddEventBottomSheetDialog extends BottomSheetDialogFragment {
+    private EditText eventNameInput,eventDateInput,noteInputEvent;
+    private Spinner spinnerReminder;
+    private Button buttonAdd;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+}
